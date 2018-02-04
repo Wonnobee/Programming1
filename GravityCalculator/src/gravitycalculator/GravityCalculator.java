cd8e712b1a658c2fec798d6a5c87e92c6649f62a
Wonnobee <mic.saltat@gmail.com>
2018-02-04 22:30:34 +0100

    GravityCalculator, computing the distance in meter depend of the time.

---
+++ GravityCalculator/src/gravitycalculator/GravityCalculator.java
@@ -0,0 +1,41 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package gravitycalculator;
+
+/**
+ *
+ * @author micsa
+ */
+import java.io.*;
+
+public class GravityCalculator {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        double  gravity = -9.81; // Earth's gravity in m/s^2
+
+        double  initialVelocity = 0.0; // starting velocity of the object
+
+	double  fallingTime = 10.0; // time in seconds that the object falls
+
+	double  initialPosition = 1000.0; // Starting position in meters, the calculation will 		determine the ending position in meters
+
+	double  finalPosition;
+        
+        // Notation: x(t) = 0.5 × at^2 + vit + xi 
+                
+        finalPosition = 01  Qº.5*gravity*fallingTime*fallingTime+initialVelocity*fallingTime+initialPosition;
+        
+        // double result = finalPosition / fallingTime;
+                
+
+	System.out.println("The object's position after " + fallingTime +" seconds is 			"+finalPosition + " m.");
+
+    }
+    
+}
